10469662755ad4fa676b79d2750796e2b690e49c
Abhishek Chaurasiya <abhichek2580@gmail.com>
2023-04-05 22:47:15 +0530

    okay

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 })
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
     origin: FRONTEND_URL,
